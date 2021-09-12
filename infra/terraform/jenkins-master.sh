f73026984e46abe42f946f11c989f5df6f25314c
Roberto Alanis <josera.baez@gmail.com>
2021-09-12 14:56:06 -0700

    infra: add terraform to install jenkins

---
+++ infra/terraform/jenkins-master.sh
@@ -0,0 +1,25 @@
+# Jenkins Installation
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo 'deb https://pkg.jenkins.io/debian-stable binary/' | sudo tee /etc/apt/sources.list
+sudo apt update
+sudo apt install -y maven openjdk-8-jre openjdk11-jdk
+sudo apt install -y jenkins
+sudo service jenkins start
+sudo service jenkins status
+
+
+
+# Ansible Installation
+sudo apt update -y
+sudo apt install ansible -y
+
+echo "${tls_private_key.private-key.private_key_pem}" > ~/.ssh/jenkins.pem
+chmod 600 ~/.ssh/jenkins.pem
+sudo sed -i '71s/.*/host_key_checking = False/' /etc/ansible/ansible.cfg
+
+cat << EOF >> hosts
+[jenkins]
+${aws_instance.jenkins.*.public_dns[1]}
+${aws_instance.jenkins.*.public_dns[2]}
+EOF
---
+++ infra/terraform/main.tf
@@ -0,0 +1,118 @@
+provider "aws" {
+  region                  = "us-east-1"
+  access_key              = var.access_key
+  secret_key              = var.secret_key
+  token                   = var.session_token
+}
+
+resource "tls_private_key" "private-key" {
+  algorithm   = "RSA"
+  rsa_bits    = 2048
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = tls_private_key.private-key.public_key_openssh
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_security_group" "jenkins-sg" {
+  name = "Jenkins-SG"
+  description = "Student security group"
+
+  tags = {
+    Name = "Jenkins-SG"
+    Environment = terraform.workspace
+  }
+}
+
+resource "aws_security_group_rule" "create-sgr-ssh" {
+  security_group_id = aws_security_group.jenkins-sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "create-sgr-inbound" {
+  security_group_id = aws_security_group.jenkins-sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  protocol          = "all"
+  to_port           = 65535
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "create-sgr-outbound" {
+  security_group_id = aws_security_group.jenkins-sg.id
+  cidr_blocks         = ["0.0.0.0/0"]
+  from_port         = 0
+  protocol          = "all"
+  to_port           = 65535
+  type              = "egress"
+}
+
+resource "aws_instance" "jenkins" {
+  count         = 3 
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  security_groups = ["Jenkins-SG"]
+  tags = {
+    Name = "Jenkins${count.index}"
+  }
+}
+
+resource "null_resource" "jenkins-master" {
+    depends_on = [aws_instance.jenkins]
+  
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = tls_private_key.private-key.private_key_pem
+      host        = aws_instance.jenkins.*.public_dns[0]
+    }
+
+    provisioner "remote-exec" {
+      script = "./jenkins-master.sh"
+    }
+
+    provisioner "local-exec" {
+      command = "echo '${tls_private_key.private-key.private_key_pem}' > ~/.ssh/jenkins.pem && chmod 600 ~/.ssh/jenkins.pem "
+    }
+}
+
+resource "null_resource" "jenkins-node" {
+    depends_on = [aws_instance.jenkins]
+  
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = tls_private_key.private-key.private_key_pem
+      host        = aws_instance.jenkins.*.public_dns[0]
+    }
+
+    provisioner "remote-exec" {
+      script = "./jenkins-node.sh"
+    }
+
+    provisioner "local-exec" {
+      command = "echo '${tls_private_key.private-key.private_key_pem}' > ~/.ssh/jenkins.pem && chmod 600 ~/.ssh/jenkins.pem "
+    }
+}
