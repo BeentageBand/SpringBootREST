a172f91494b0d3e10c180d686fdd8a17173fbc4b
Roberto Alanis <josera.baez@gmail.com>
2021-09-18 17:44:48 -0700

    add jenkins.pem to JENKINS_HOME

--- infra/terraform/jenkins-master.sh
+++ infra/terraform/jenkins-master.sh
@@ -6,6 +6,10 @@ echo 'deb https://pkg.jenkins.io/debian-stable binary/' | sudo tee /etc/apt/sour
 sudo apt update -y
 sudo apt install -y maven openjdk-8-jre openjdk-11-jdk
 sudo apt install -y jenkins
+
 sudo service jenkins start
 
+# setup .ssh
+sudo cp -r ~/.ssh /var/lib/jenkins/
+sudo chown jenkins -R /var/lib/jenkins/.ssh
 
